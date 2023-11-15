25e1fc49844864e589cb28fcd0b91e548703cdfc
pokeskive <140730150+pokeskive@users.noreply.github.com>
2023-11-15 19:29:20 +0200

    Delete lua/plugins.lua

--- lua/plugins.lua
+++
@@ -1,8 +0,0 @@
-return require('packer').startup(function(use)
-  -- Packer can manage itself
-  use 'wbthomason/packer.nvim'
-  use 'terrortylor/nvim-comment'
-  use 'm4xshen/autoclose.nvim'
-	use "Alexis12119/nightly.nvim"
-	use {'nvim-lualine/lualine.nvim', requires = { 'kyazdani42/nvim-web-devicons', opt = true } }
-end)
